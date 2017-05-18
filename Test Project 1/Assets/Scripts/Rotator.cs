2d5c3059ea91cc3f8d6a385a5f1dbcffc4eee9e6
Soham Dongargaonkar <sohamssd@gmail.com>
2017-05-18 19:43:37 +0530

    Add collectibles as prefabs

---
+++ Test Project 1/Assets/Scripts/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+	public GameObject collectible;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (60, 30, 90) * Time.deltaTime);
+	}
+}
+	
